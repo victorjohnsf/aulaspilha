7061cec17b61d8868304fa7bca88421296655c0e
victorjohnsf <103009841+victorjohnsf@users.noreply.github.com>
2023-10-24 20:59:21 -0300

    Add files via upload
    Exercício utilizando pilha

---
+++ pilha2.java
@@ -0,0 +1,44 @@
+package exepilha;
+
+public class pilha2 {
+	public static void main(String[] args) {
+		pilha p1 = new pilha();
+		int topo = -1;
+		String caracter ;
+		
+		
+		topo = p1.push(topo, "E");
+		topo = p1.push(topo, "S");
+		topo = p1.push(topo, "T");
+		topo = p1.push(topo, "E");
+		topo = p1.push(topo, " ");
+		topo = p1.push(topo, "E");
+		topo = p1.push(topo, "X");
+		topo = p1.push(topo, "E");
+		topo = p1.push(topo, "R");
+		topo = p1.push(topo, "C");
+		topo = p1.push(topo, "I");
+		topo = p1.push(topo, "C");
+		topo = p1.push(topo, "I");
+		topo = p1.push(topo, "O");
+		topo = p1.push(topo, " ");
+		topo = p1.push(topo, "E");
+		topo = p1.push(topo, " ");
+		topo = p1.push(topo, "M");
+		topo = p1.push(topo, "U");
+		topo = p1.push(topo, "I");
+		topo = p1.push(topo, "T");
+		topo = p1.push(topo, "O");
+		topo = p1.push(topo, " ");
+		topo = p1.push(topo, "F");
+		topo = p1.push(topo, "A");
+		topo = p1.push(topo, "C");
+		topo = p1.push(topo, "I");
+		topo = p1.push(topo, "L");
+		
+		
+		p1.imprimir(topo);
+		
+		
+	}
+}
